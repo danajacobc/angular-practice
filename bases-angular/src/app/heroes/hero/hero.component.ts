c076c39c77f6e966d783896bfdf43e15c6676ca3
danajacobc <danajacobc@gmail.com>
2024-02-17 14:33:44 -0300

    one way data binding

--- bases-angular/src/app/heroes/hero/hero.component.ts
+++ bases-angular/src/app/heroes/hero/hero.component.ts
@@ -9,5 +9,12 @@ export class HeroComponent {
   public name: string = 'ironman';
   public age: number = 45;
 
+   get capitalizedName(): string { //los getters se ven como propiedad, pero son métodos.
+      return this.name.toUpperCase();
+   }
+
+   getHeroDescription(): string {  //método
+    return `${this.name} - ${this.age}`;
+   }
 
 }
