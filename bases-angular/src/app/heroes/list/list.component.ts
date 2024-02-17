43a0da47c7484c0cbd118e1c2c4ea6d2e3e05b65
danajacobc <danajacobc@gmail.com>
2024-02-17 17:12:28 -0300

    ngFor

--- bases-angular/src/app/heroes/list/list.component.ts
+++ bases-angular/src/app/heroes/list/list.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  public heroNames: string[] = ['Spider-Man', 'Iron Man', 'Capitana Marvel', 'Thor', 'Hulk', 'Black Widow', 'Wolverine', 'Capitán América']
+
 
 }
