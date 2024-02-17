45edd8deaaae9c2b28dd851bd659f6c44c730ef7
danajacobc <danajacobc@gmail.com>
2024-02-17 18:01:08 -0300

    heroes module

--- bases-angular/src/app/app.module.ts
+++ bases-angular/src/app/app.module.ts
@@ -3,21 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 
 import { CounterModule } from './counter/components/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent,
-    ListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CounterModule
+    CounterModule,
+    HeroesModule,
   ],
   providers: [
     provideClientHydration()
