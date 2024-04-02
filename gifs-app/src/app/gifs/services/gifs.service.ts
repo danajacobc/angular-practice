468f1fea12883508c81c07728a706ae2e0444e0e
danajacobc <danajacobc@gmail.com>
2024-04-02 18:22:34 -0300

    pet http, interfaces

--- gifs-app/src/app/gifs/services/gifs.service.ts
+++ gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
+  public gifsList: Gif[] = [];
+
   private _tagsHistory: string[] = [];
   private apiKey: string = 'vpzLdV3iv2ttHr1t0jknOMrcHZQkdYZ8';
-  private serviceUrl: string = 'https://api.giphy.com/v1/gifs/'
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs/';
 
   constructor( private http: HttpClient ) { }
 
@@ -36,10 +39,9 @@ export class GifsService {
       .set('limit', '10')
       .set('q', tag)
 
-    this.http.get(`${this.serviceUrl}search`, { params })
-      .subscribe( resp => {
-        console.log(resp);
-
+    this.http.get<SearchResponse>(`${this.serviceUrl}search`, { params })
+      .subscribe( (resp) => {
+        this.gifsList = resp.data;
       });
 
     // fetch(`https://api.giphy.com/v1/gifs/search?api_key=vpzLdV3iv2ttHr1t0jknOMrcHZQkdYZ8&q=${tag}&limit=10`)
