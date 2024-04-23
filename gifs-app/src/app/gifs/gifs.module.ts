0a75991876f1d55463b37787ec6e28ccc497c073
danajacobc <danajacobc@gmail.com>
2024-04-23 19:04:45 -0300

    lazy image

--- gifs-app/src/app/gifs/gifs.module.ts
+++ gifs-app/src/app/gifs/gifs.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomePageComponent } from './pages/home/home-page.component';
-import { SearchBoxComponent } from './components/search-box/search-box.component';
+
+import { SharedModule } from '../shared/shared.module';
+
 import { CardListComponent } from './components/card-list/card-list.component';
 import { GifCardComponent } from './components/gif-card/gif-card.component';
-
+import { HomePageComponent } from './pages/home/home-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 
 @NgModule({
   declarations: [
+    CardListComponent,
+    GifCardComponent,
     HomePageComponent,
     SearchBoxComponent,
-    CardListComponent,
-    GifCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     HomePageComponent,
