dc42be454734abc8d2dcd38dcb6d3115ded93b3d
danajacobc <danajacobc@gmail.com>
2024-05-21 10:33:06 -0300

    country page

--- country-app/src/app/countries/pages/country-page/country-page.component.ts
+++ country-app/src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -9,6 +10,8 @@ import { switchMap } from 'rxjs';
   styles: ``
 })
 export class CountryPageComponent implements OnInit {
+
+  public country?: Country;
 
   constructor(
       private activateRoute: ActivatedRoute,
@@ -27,8 +30,7 @@ export class CountryPageComponent implements OnInit {
           return this.router.navigateByUrl('');
         }
 
-        console.log({country})
-        return;
+        return this.country = country;
       })
   }
 
