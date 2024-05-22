728b7ba1628dcc1134a1c735604626f39fb8ca4d
danajacobc <danajacobc@gmail.com>
2024-05-22 17:16:24 -0300

    reg. page

--- formApp/src/app/auth/pages/register-page/register-page.component.ts
+++ formApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
   styles: ``
 })
 export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  isValidField(field: string) {}
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 }
