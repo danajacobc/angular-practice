abedf692e4e3a2d835c0ade59e68f5b3bc78eba0
danajacobc <danajacobc@gmail.com>
2024-05-22 14:33:43 -0300

    add fav

--- formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -14,6 +14,8 @@ export class DynamicPageComponent {
       ['Death Stranding', Validators.required ],
     ]),
   });
+
+  public newFavorite: FormControl = new FormControl('', Validators.required );
 
   constructor( private fb: FormBuilder ) {}
 
@@ -48,6 +50,16 @@ export class DynamicPageComponent {
     return null;
   }
 
+  onAddToFavorites(): void {
+    if(this.newFavorite.invalid) return;
+    const newGame = this.newFavorite.value;
+
+    this.favoriteGames.push(
+      this.fb.control(newGame, Validators.required )
+    );
+    this.newFavorite.reset();
+  }
+
   onDeleteFavorite( index : number ): void {
     this.favoriteGames.removeAt(index);
   }
@@ -60,6 +72,7 @@ export class DynamicPageComponent {
     }
 
     console.log(this.myForm.value);
+    (this.myForm.controls['favoriteGames'] as FormArray) = this.fb.array([]);
     this.myForm.reset();
 
   }
